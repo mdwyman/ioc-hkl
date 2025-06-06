1206c4db77f62607a87f360b0eadfad3b505bc58
mwyman <mwyman@anl.gov>
2025-06-06 16:22:03 -0500

    Setting up for APS tests

--- iocBoot/iocpydev/st.cmd
+++ iocBoot/iocpydev/st.cmd
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-export GI_TYPELIB_PATH=/usr/local/lib/girepository-1.0
+source 100idHkl_startup_env
+
+export GI_TYPELIB_PATH=/home/beams2/MWYMAN/micromamba/envs/bluesky_2024_1/envs/hkl/lib/girepository-1.0
 # export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
 
-../../bin/linux-x86_64/hklApp st_base.cmd
+../../bin/rhel9-x86_64/hklApp st_base.cmd
 
--- iocBoot/iocpydev/st_base.cmd
+++ iocBoot/iocpydev/st_base.cmd
@@ -6,7 +6,7 @@
 epicsEnvSet("PYTHONPATH","$(TOP)/python")
 
 # Designate beamline:IOC_name as conventional prefix
-epicsEnvSet("PREFIX", "abtest:ioc-hkl:")
+epicsEnvSet("PREFIX", "100id:ioc-hkl")
 
 cd ${TOP}
 
@@ -15,7 +15,7 @@ dbLoadDatabase "${TOP}/dbd/hklApp.dbd"
 hklApp_registerRecordDeviceDriver pdbbase
 
 ## Load record instances
-dbLoadRecords("db/hkl_main.db")
+dbLoadRecords("db/hkl_main.db", "PREFIX=$(PREFIX)")
 
 cd ${TOP}/iocBoot/${IOC}
 
@@ -26,36 +26,36 @@ iocInit
 
 epicsThreadSleep(1)
 
-dbpf("$(PREFIX)geom","2")
-dbpf("$(PREFIX)wlen","5.431")
-dbpf("$(PREFIX)latt_a","5.431")
-dbpf("$(PREFIX)latt_b","5.431")
-dbpf("$(PREFIX)latt_c","5.431")
-dbpf("$(PREFIX)latt_alpha","90")
-dbpf("$(PREFIX)latt_beta","90")
-dbpf("$(PREFIX)latt_gamma","90")
-dbpf("$(PREFIX)omega_e4c","30")
-dbpf("$(PREFIX)chi_e4c","20")
-dbpf("$(PREFIX)phi_e4c","10")
-dbpf("$(PREFIX)tth_e4c","10")
-dbpf("$(PREFIX)h","1")
-dbpf("$(PREFIX)k","1")
-dbpf("$(PREFIX)l","1")
-dbpf("$(PREFIX)refl1_h_e4c","0")
-dbpf("$(PREFIX)refl1_k_e4c","0")
-dbpf("$(PREFIX)refl1_l_e4c","4")
-dbpf("$(PREFIX)refl1_omega_e4c","-145")
-dbpf("$(PREFIX)refl1_chi_e4c","0")
-dbpf("$(PREFIX)refl1_phi_e4c","0")
-dbpf("$(PREFIX)refl1_tth_e4c","60")
-dbpf("$(PREFIX)refl2_h_e4c","0")
-dbpf("$(PREFIX)refl2_k_e4c","4")
-dbpf("$(PREFIX)refl2_l_e4c","0")
-dbpf("$(PREFIX)refl2_omega_e4c","-145")
-dbpf("$(PREFIX)refl2_chi_e4c","90")
-dbpf("$(PREFIX)refl2_phi_e4c","0")
-dbpf("$(PREFIX)refl2_tth_e4c","69")
-#dbpf("$(PREFIX)errors","my string test")
+dbpf("$(PREFIX):geom","2")
+dbpf("$(PREFIX):wlen","5.431")
+dbpf("$(PREFIX):latt_a","5.431")
+dbpf("$(PREFIX):latt_b","5.431")
+dbpf("$(PREFIX):latt_c","5.431")
+dbpf("$(PREFIX):latt_alpha","90")
+dbpf("$(PREFIX):latt_beta","90")
+dbpf("$(PREFIX):latt_gamma","90")
+dbpf("$(PREFIX):omega_e4c","30")
+dbpf("$(PREFIX):chi_e4c","20")
+dbpf("$(PREFIX):phi_e4c","10")
+dbpf("$(PREFIX):tth_e4c","10")
+dbpf("$(PREFIX):h","1")
+dbpf("$(PREFIX):k","1")
+dbpf("$(PREFIX):l","1")
+dbpf("$(PREFIX):refl1_h_e4c","0")
+dbpf("$(PREFIX):refl1_k_e4c","0")
+dbpf("$(PREFIX):refl1_l_e4c","4")
+dbpf("$(PREFIX):refl1_omega_e4c","-145")
+dbpf("$(PREFIX):refl1_chi_e4c","0")
+dbpf("$(PREFIX):refl1_phi_e4c","0")
+dbpf("$(PREFIX):refl1_tth_e4c","60")
+dbpf("$(PREFIX):refl2_h_e4c","0")
+dbpf("$(PREFIX):refl2_k_e4c","4")
+dbpf("$(PREFIX):refl2_l_e4c","0")
+dbpf("$(PREFIX):refl2_omega_e4c","-145")
+dbpf("$(PREFIX):refl2_chi_e4c","90")
+dbpf("$(PREFIX):refl2_phi_e4c","0")
+dbpf("$(PREFIX):refl2_tth_e4c","69")
+#dbpf("$(PREFIX):errors","my string test")
 
 
 dbl > pvlist.dbl
